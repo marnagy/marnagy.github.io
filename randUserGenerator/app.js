101f219c7009a7e9221f61f5ab184ff173e88ed3
Marek Nagy <62247071@cuni.cz>
2021-03-30 15:09:16 +0200

    Added Vue.js random person generator

---
+++ randUserGenerator/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+            const person = results[0]
+
+            console.log(person)
+            this.firstName = person.name.first
+            this.lastName = person.name.first
+            this.email = person.email
+            this.gender = person.gender
+            this.picture = person.picture.large
+        },
+    },
+});
+
+app.mount('#app')
