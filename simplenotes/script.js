ddababcd9bb9869e159dd3d58b461f0a393d44da
Marek Nagy <62247071@cuni.cz>
2021-04-21 20:03:21 +0200

    Added WORKING login

--- simplenotes/script.js
+++ simplenotes/script.js
@@ -4,32 +4,52 @@ let resp = null
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://192.168.2.23:5000',
+            token: '',
             title: 'Simple Notes App',
-            username: 'test',
-            password: 'password',
+            username: '',
+            password: '',
         }
     },
     methods: {
+        build_ui(){
+            const login_form = document.getElementById('login_form')
+            login_form.style.display = 'none'
+        },
         async login() {
             const method = 'POST'
-            const url = 'https://simplenotes-app.herokuapp.com/login'
+            //const url = 'https://simplenotes-app.herokuapp.com/login'
+            const login_url = this.url + '/api/login'
             const json = {'username': this.username, 'password': this.password}
-
-            console.log(json);
             
-            const response = await fetch(url, {
+            const response = await fetch(login_url, {
                 method: method,
                 mode: 'cors',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(json)
             })
-            console.log(response)
-            headers = response.headers
+
             const resp_json = await response.json()
-            console.log(resp_json)
-            // HOW DO I GET SESSIONID ??
+            this.token = resp_json.access_token
+
+            this.build_ui()
+        },
+        async hello(){
+            const response = await fetch(this.url + '/auth/test')
+            const json = await response.json()
+            console.log(json)
         },
         async check_login(){
-            const response = await fetch('https://simplenotes-app.herokuapp.com/login')
+            const response = await fetch(this.url + '/auth/test',{
+                method: 'GET',
+                mode: 'cors',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'Authentication': 'JWT ' + this.token
+                }
+            })
             const json = await response.json()
             console.log(json)
         },
